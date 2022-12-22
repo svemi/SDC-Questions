778f2e65c932d88dcb7835ba9013130f6789f81f
Sai Vemireddy <srisaiteja@gmail.com>
2022-12-22 14:38:27 -0800

    created functional sql file to create database and pertinent tables

--- databases/qaDB.sql
+++ databases/qaDB.sql
@@ -2,41 +2,42 @@ DROP DATABASE IF EXISTS qaDB;
 
 CREATE DATABASE qaDB;
 
-USE qaDB;
+\c qadb;
 
+DROP TABLE IF EXISTS product CASCADE;
 
 CREATE TABLE product (
-  product_id varchar(10) unique primary key,
-  results: questions[]
-
+  product_id varchar(10) primary key
 );
-CREATE TYPE questions (
-  question_id integer unique primary key,
+
+DROP TABLE IF EXISTS questions CASCADE;
+
+CREATE TABLE questions (
+  question_id serial primary key,
+  product_id varchar(10) references product(product_id),
   question_body text,
-  question_data date,
+  question_data timestamptz,
   asker_name varchar(60),
   question_helpfullness integer default 0,
-  reported boolean default 0,
-  answers answers[];
+  reported boolean default false
 );
 
-CREATE TYPE answers (
-  answer_id integer unique primary key,
+DROP TABLE IF EXISTS answers CASCADE;
+
+CREATE TABLE answers (
+  answer_id serial primary key,
+  question_id serial references questions(question_id),
   body text,
-  date date,
-  answerer_name: varchar(50);
-  helpfullness integer default 0;
-  reported boolean default 0,
-  photos photos[];
-
+  date timestamptz,
+  answerer_name varchar(50),
+  helpfullness integer default 0,
+  reported boolean default false
 );
 
-CREATE TYPE photos (
-  id integer unique primary key,
+DROP TABLE IF EXISTS photos CASCADE ;
+
+CREATE TABLE photos (
+  id serial PRIMARY KEY,
+  answer_id serial references answers(answer_id),
   url text
-)
-
-
-
-
-
+);
