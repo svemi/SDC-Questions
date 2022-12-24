9609d338b9cb2a6a694f5600b1cd0fdf91184e5f
Sai Vemireddy <srisaiteja@gmail.com>
2022-12-24 15:19:25 -0800

    created streams to load questions, answers, and answer photos information

--- databases/qapg.js
+++ databases/qapg.js
@@ -12,16 +12,33 @@ const pool = new Pool({
   port: 5432
 });
 
-var csvPath = path.join(__dirname, '/../data/questions_test.csv');
-fs.createReadStream(csvPath)
+var questionsPath = path.join(__dirname, '/../data/questions.csv');
+var answersPath = path.join(__dirname, '/../data/answers.csv');
+var answerPhotosPath = path.join(__dirname, '/../data/answers_photos.csv');
+
+// LOAD QUESTIONS DATA INTO QUESTIONS
+
+fs.createReadStream(questionsPath)
 .pipe(csv())
 .on('data', (row) => {
-  // let productInsertQuery = 'INSERT INTO product (product_id) VALUES ($1)';
-  // pool.query(productInsertQuery, [row.product_id], (err, res) => {if (err) {console.log(err)}} )
   let questionInsertQuery = `INSERT INTO questions (question_id, product_id, question_body, question_date, asker_name, reported, question_helpfullness) VALUES ($1, $2, $3, $4, $5, $6, $7)`;
-  pool.query(questionInsertQuery, [row.id, row.product_id, row.body, new Date(Number(row.date_written)), row.asker_name, row.reported, row.helpful], (err, res) => {if (err) {console.log(err)}});
+  pool.query(questionInsertQuery, [row.id, row.product_id, row.body, new Date(Number(row.date_written) * 1000), row.asker_name, row.reported, row.helpful], (err, res) => {if (err) {console.log(err)}});
 });
 
-// let query = `INSERT INTO questions (question_id, product_id, question_body, question_date, asker_name, reported, question_helpfullness) VALUES ($1, $2, $3, $4, $5, $6, $7)`;
-// pool.query(query, ['1', 1, 'hello world', new Date(1595884714409 * 1000), 'akser', 0, 1], (err, res) => {if (err) {console.log(err)}});
+// LOAD ANSWERS DATA INTO ANSWERS
 
+fs.createReadStream(answersPath)
+.pipe(csv())
+.on('data', (row) => {
+  let answerInsertQuery = `INSERT INTO answers (answer_id, question_id, body, date, answerer_name, reported, helpfullness) VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+  pool.query(questionInsertQuery, [row.id, row.question_id, row.body, new Date(Number(row.date_written) * 1000), row.answerer_name, row.reported, row.helpful], (err, res) => {if (err) {console.log(err)}});
+});
+
+// LOAD ANSWER PHOTOS DATA INTO PHOTOS
+
+fs.createReadStream(answerPhotosPath)
+.pipe(csv())
+.on('data', (row) => {
+  let answerPhotosInsertQuery = `INSERT INTO photos (id, answer_id, url) VALUES ($1, $2, $3)`;
+  pool.query(questionInsertQuery, [row.id, row.answer_id, row.url], (err, res) => {if (err) {console.log(err)}});
+});
