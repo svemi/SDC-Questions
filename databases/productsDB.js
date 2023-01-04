7e0899c054a654c27db55ff789f4be364f36c91f
Chris Wong <wongcpie@gmail.com>
2023-01-04 09:02:42 -1000

    working DB, copied CSV files, queries

--- databases/productsDB.js
+++ databases/productsDB.js
@@ -1,40 +1,76 @@
-
-const { Pool, Client } = require('pg')
+const { Pool } = require('pg')
+//require('dotenv').config();
 
 const pool = new Pool({
-  user: 'root',
+  // user: process.env.USERNAME || '',
+  // database: process.env.DBNAME,
+  // password: process.env.PASSWORD,
+  // host: process.env.HOST,
+  port: 5432,
+  // user: 'root',
+  user: 'christopherwong',
   host: 'localhost',
-  database: 'productsDB',
+  database: 'productsdb',
   password: '',
-  port: 3000,
+  // port: 3000,
 })
 
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
+const connection = {
+  pool,
+  query: (...args) => {
+    return pool.connect().then((client) => {
+      return client.query(...args).then((res) => {
+        client.release();
+        return res.rows;
+      });
+    });
+  },
+};
 
-const client = new Client({
-  user: 'root',
-  host: 'localhost',
-  database: 'productsDB',
-  password: '',
-  port: 3000,
-})
-client.connect()
+module.exports = connection;
 
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
+// pool.connect()
+// .then(()=>{console.log('connection sucessful')})
+// .catch((e)=>{console.log(e)})
+// .finally(()=>{pool.end()})
 
-const pg = require('pg');
-module.exports = pg.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'productsDB'
-});
+// pool.connect()
+
+// const query = (text, params, callback) => {
+//   return pool.query(text, params, callback)
+// }
+
+// module.exports = { query }
+
+// pool.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   pool.end()
+// })
+
+
+// const pg = require('pg');
+// module.exports = pg.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'productsDB'
+// });
+// client is static, pool is dynamic can change
+// const client = new Client({
+//   user: 'root',
+//   host: 'localhost',
+//   database: 'productsDB',
+//   password: '',
+//   port: 3000,
+// })
+// client.connect()
+
+// client.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   client.end()
+// })
+
+
 
 // const {PG} = require('pg');
 // const pg = new PG ({
